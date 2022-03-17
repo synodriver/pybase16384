ad7651b196d74160d619b46282ba2d78d028f19a
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-18 00:06:56 +0800

    feat: add printable api & edit README

--- pybase16384/__init__.py
+++ pybase16384/__init__.py
@@ -11,8 +11,16 @@ def encode_from_string(data: str, write_head: bool = False) -> bytes:
     return out.getvalue()
 
 
+def encode_to_string(data: bytes) -> str:
+    return encode(data).decode("utf-16-be")
+
+
 def decode_from_bytes(data: bytes) -> str:
     inp = BytesIO(data)
     out = BytesIO()
     decode_file(inp, out)
     return out.getvalue().decode()
+
+
+def decode_from_string(data: str) -> bytes:
+    return decode(data.encode("utf-16-be"))
