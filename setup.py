96725ec54a6bff62fbda66e723bc4fe77785d047
synodriver <diguohuangjiajinweijun@gmail.com>
2024-03-26 20:22:47 +0800

    add ci build wheel

--- setup.py
+++ setup.py
@@ -149,6 +149,7 @@ def main():
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
         ],
